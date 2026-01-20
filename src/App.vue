27e3f36f3cdc687ad2e2408594d73f9d48bfca5d
答案 <1259558183@qq.com>
2026-01-20 15:35:52 +0800

    style: 调整弹框内容文字

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,13 @@
   </el-config-provider>
 </template>
 <script setup lang="ts">
-import DisableDevtool from "disable-devtool";
-import { ElNotification } from "element-plus";
-import { onMounted } from "vue";
+import DisableDevtool from 'disable-devtool';
+import { ElNotification } from 'element-plus';
+import { onMounted } from 'vue';
 const env = import.meta.env.VITE_USER_NODE_ENV;
 
 onMounted(async () => {
-  if (env === "production") {
+  if (env === 'production') {
     DisableDevtool({
       disableMenu: true, // 禁用右键菜单
       disableSelect: true, // 禁用选择
@@ -23,12 +23,12 @@ onMounted(async () => {
       },
     });
     ElNotification.success({
-      title: '3D模型场景编辑器(Vue3/React18)',
+      title: 'ThreeFlowX(商用版)',
       dangerouslyUseHTMLString: true,
       message: `<div>
         <div>提供高度灵活便捷化的3D模型场景操作编辑功能。</div>
         <div style="margin-top: 10px;">
-           <a href="https://three3d-0gte3eg619c78ffd-1301256746.tcloudbaseapp.com/threejs-3dscene-docs/" target="_blank">查看详情</a>
+           <a href="http://threeflowx.cn/edit/" target="_blank">查看详情</a>
         </div>
     </div>`,
       duration: 0,
