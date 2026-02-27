17b0189dba0c25be4910ad5efdcc1a0f20c4fda7
答案 <1259558183@qq.com>
2026-02-27 20:57:27 +0800

    chore: 添加常量注释

--- src/config/constant.ts
+++ src/config/constant.ts
@@ -1,9 +1,14 @@
+// 预览地址
 export const PREVIEW_URL = '/preview';
 
+// 雾近值
 export const FOG_NEAR_VALUE = 1;
 
+// 雾远值
 export const FOG_FAR_VALUE = 200;
 
+// 雾密度值
 export const FOG_DENSITY_VALUE = 0.001;
 
+// 雾颜色值
 export const FOG_COLOR_VALUE = '#EFD1B5';
